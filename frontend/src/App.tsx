55b6f9bf4807e98e6bd5a3cb8b2c6338c15357be
Brendan Nyambati <elkhead.smc@gmail.com>
2026-06-17 14:00:11 -0500

    added dummy fetch

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,20 @@
 
+import { useEffect, useState } from 'react';
+
 const App = () => {
+  const [data, setData] = useState<any>(null);
+
+  useEffect(() => {
+    fetch('http://localhost:3000/dummy')
+      .then((res) => res.json())
+      .then((data) => setData(data))
+      .catch((err) => console.error(err));
+  }, []);
+
   return (
-    <p>Looks like it's working.</p>
+    <>
+      {data && <pre>{JSON.stringify(data, null, 2)}</pre>}
+    </>
   );
 };
 
