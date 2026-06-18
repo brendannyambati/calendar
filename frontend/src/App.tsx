99c64d56ef82530725f1bdeb152ec3dc39977c58
Brendan Nyambati <elkhead.smc@gmail.com>
2026-06-18 15:28:19 -0500

    Added mui. created calendar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,21 +1,8 @@
 
-import { useEffect, useState } from 'react';
+import { Calendar } from './components/calendar/Calendar';
 
 const App = () => {
-  const [data, setData] = useState<any>(null);
-
-  useEffect(() => {
-    fetch(`${import.meta.env.VITE_API_URL}/dummy`)
-      .then((res) => res.json())
-      .then((data) => setData(data))
-      .catch((err) => console.error(err));
-  }, []);
-
-  return (
-    <>
-      {data && <pre>{JSON.stringify(data, null, 2)}</pre>}
-    </>
-  );
+  return (<Calendar />);
 };
 
 export default App;
